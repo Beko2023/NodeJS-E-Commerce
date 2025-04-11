934903da7046286554db461bfa8ee5157950a541
Beko2023 <139360217+Beko2023@users.noreply.github.com>
2025-04-11 14:53:27 +0300

    4/11

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,17 @@
 const User = require("../models/user");
+const nodemailer = require("nodemailer");
+
 const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+
+const transporter = nodemailer.createTransport({
+  host: "live.smtp.mailtrap.io",
+  port: 587,
+  auth: {
+    user: "mberkozturkk@gmail.com",
+    pass: "357703b5317f16ad6628e5c9cffcd2aa", //need to set up password through security.
+  },
+});
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
@@ -37,37 +49,39 @@ exports.postSignup = (req, res, next) => {
       });
     })
     .then((result) => {
-      res.redirect("/login");
+      return transporter
+        .sendMail({
+          from: '"your app" <mberkozturkk@gmail.com>',
+          to: email,
+          subject: "Sign Up Succesful",
+          html: "<h1>Welcome to our shop!</h1>",
+        })
+        .then(() => res.redirect("/login"));
     })
     .catch((err) => console.log(err));
 };
 
 exports.postLogin = (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  User.findOne({ email: email })
+  const { email, password } = req.body;
+
+  User.findOne({ email })
     .then((user) => {
       if (!user) {
         req.flash("error", "Invalid email or password");
         return res.redirect("/login");
       }
-      bcrypt
-        .compare(password, user.password)
-        .then((doMatch) => {
-          if (doMatch) {
-            req.session.isLoggedIn = true;
-            req.session.userId = user._id;
-            return req.session.save((err) => {
-              console.log(err);
-              return res.redirect("/");
-            });
-          }
-          res.redirect("/login");
-        })
-        .catch((err) => {
-          console.log(err);
-          res.redirect("/login");
-        });
+      return bcrypt.compare(password, user.password).then((doMatch) => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save((err) => {
+            console.log(err);
+            res.redirect("/");
+          });
+        }
+        req.flash("error", "Invalid email or password");
+        res.redirect("/login");
+      });
     })
     .catch((err) => console.log(err));
 };
@@ -76,5 +90,39 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
     res.redirect("/");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Password Reset",
+    errorMessage: req.flash("error"),
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with that email found.");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        //send email here.
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 };
